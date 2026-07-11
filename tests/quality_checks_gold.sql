3a7b8a73634fec588e12d4a323d046f556b59215
doozietam <doozietam06@gmail.com>
2026-07-11 15:45:18 +0530

    Add quality checks for gold layer data validation
    This script validates the quality and consistency of the data in the 'gold' layer, including checks for uniqueness of surrogate keys and referential integrity.

---
+++ tests/quality_checks_gold.sql
@@ -0,0 +1,55 @@
+/*
+===============================================================================
+Quality Checks
+===============================================================================
+Script Purpose:
+    This script validates the quality and consistency of the data in the
+    'gold' layer to ensure it is ready for analytics and reporting.
+
+    The validation checks include:
+    - Uniqueness of surrogate keys in dimension views.
+    - Referential integrity between fact and dimension views.
+    - Consistency of relationships within the analytical data model.
+
+Usage:
+    - Execute this script after loading the Gold layer.
+    - Review and resolve any issues identified before using the data for
+      reporting or business analysis.
+===============================================================================
+*/
+
+-- ====================================================================
+-- Checking 'gold.dim_customers'
+-- ====================================================================
+-- Check for Uniqueness of Customer Key in gold.dim_customers
+-- Expectation: No results 
+SELECT 
+    customer_key,
+    COUNT(*) AS duplicate_count
+FROM gold.dim_customers
+GROUP BY customer_key
+HAVING COUNT(*) > 1;
+
+-- ====================================================================
+-- Checking 'gold.product_key'
+-- ====================================================================
+-- Check for Uniqueness of Product Key in gold.dim_products
+-- Expectation: No results 
+SELECT 
+    product_key,
+    COUNT(*) AS duplicate_count
+FROM gold.dim_products
+GROUP BY product_key
+HAVING COUNT(*) > 1;
+
+-- ====================================================================
+-- Checking 'gold.fact_sales'
+-- ====================================================================
+-- Check the data model connectivity between fact and dimensions
+SELECT * 
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+ON c.customer_key = f.customer_key
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key
+WHERE p.product_key IS NULL OR c.customer_key IS NULL  
